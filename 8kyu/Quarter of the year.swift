38d99476b94acf11d35c2b183847504c78bd2897
Aleksandr F <a7214370@gmail.com>
2022-09-29 17:50:29 +0300

    Update Quarter of the year.swift

--- 8kyu/Quarter of the year.swift
+++ 8kyu/Quarter of the year.swift
@@ -13,3 +13,18 @@ For example: month 2 (February), is part of the first quarter; month 6 (June),
 func quarter(of month: Int) -> Int {
   return 0
 }
+
+// Solution 1
+func quarter(of month: Int) -> Int {
+    switch month {
+    case 1...3: return 1
+    case 4...6: return 2
+    case 7...9: return 3
+    case 10...12: return 4
+    default: print("Error")
+    }
+  return month
+}
+
+// Solution 2
+
