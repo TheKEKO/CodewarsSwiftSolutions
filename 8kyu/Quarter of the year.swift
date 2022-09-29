7978b0436cff62795953a06a2e5a2a0c27c8426e
Aleksandr F <a7214370@gmail.com>
2022-09-29 18:10:42 +0300

    Update Quarter of the year.swift

--- 8kyu/Quarter of the year.swift
+++ 8kyu/Quarter of the year.swift
@@ -1,14 +1,16 @@
 /* Given a month as an integer from 1 to 12, return to which quarter of the year
  it belongs as an integer number.
 
-For example: month 2 (February), is part of the first quarter; month 6 (June),
- is part of the second quarter; and month 11 (November), is part of the fourth quarter. */
+ For example: month 2 (February), is part of the first quarter; 
+ month 6 (June), is part of the second quarter; 
+ and month 11 (November), is part of the fourth quarter. */
 
-/* Задав месяц как целое число от 1 до 12, верните, к какому кварталу года оно
+/* Задав месяц как целое число от 1 до 12, верните, к какому кварталу года он
  относится как целое число.
  
- Например: месяц 2 (февраль) относится к первому кварталу; месяц 6 (июнь) - ко второму
- кварталу; месяц 11 (ноябрь) - к четвертому кварталу. */
+ Например: месяц 2 (февраль) относится к первому кварталу; 
+ месяц 6 (июнь) - ко второму кварталу; 
+ месяц 11 (ноябрь) - к четвертому кварталу. */
 
 func quarter(of month: Int) -> Int {
   return 0
@@ -27,4 +29,17 @@ func quarter(of month: Int) -> Int {
 }
 
 // Solution 2
-
+func quarter(of month: Int) -> Int {
+    if 1...3 ~= month {
+        return 1
+    } else if 4...6 ~= month {
+        return 2
+    } else if 7...9 ~= month {
+        return 3
+    } else if 10...12 ~= month {
+        return 4
+    } else {
+        print("Error")
+    }
+  return month
+}
