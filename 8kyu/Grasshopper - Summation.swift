357b622b9890259f21fbc7f141ea130d6aa8084a
Aleksandr F <a7214370@gmail.com>
2022-09-30 09:31:21 +0300

    Update Grasshopper - Summation.swift

--- 8kyu/Grasshopper - Summation.swift
+++ 8kyu/Grasshopper - Summation.swift
@@ -5,7 +5,19 @@ func summation(_ n: Int) -> Int {
     // your code here
 }
 
-// Solution
+// Solution 1
 func summation(_ n: Int) -> Int {
     return n * (n + 1) / 2
+}
+
+// Solution 2
+func summation(_ n: Int) -> Int {
+    var result = 0
+    var number = n
+    
+    while number >= 1 {
+        result += number
+        number -= 1
+    }
+    return result
 }
