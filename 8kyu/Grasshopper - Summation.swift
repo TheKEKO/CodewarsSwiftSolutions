b94fff36ed1b4fc951555c9d880204a84c8a76e1
Aleksandr F <a7214370@gmail.com>
2022-09-30 09:32:32 +0300

    Update Grasshopper - Summation.swift

--- 8kyu/Grasshopper - Summation.swift
+++ 8kyu/Grasshopper - Summation.swift
@@ -1,3 +1,5 @@
+// Grasshopper - Summation
+
 // Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
 // Напишите программу, которая находит сумму всех чисел от 1 до num. Число всегда будет целым положительным числом больше 0.
 
