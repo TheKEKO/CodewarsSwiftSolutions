fa183262e2f072bf12199c22cab35bf9d3a4622c
Aleksandr F <a7214370@gmail.com>
2022-10-01 23:33:38 +0300

    Update Reversed Strings.swift

--- 8kyu/Reversed Strings.swift
+++ 8kyu/Reversed Strings.swift
@@ -1,7 +1,13 @@
 // Reversed Strings
 
 // Complete the solution so that it reverses the string passed into it.
+// Завершите решение так, что бы оно изменило направление переданной в него строки.
 
 func reverse(_ str: String) -> String {
     return ""
+}
+
+// Solution
+func reverse(_ str: String) -> String {
+    return String(str.reversed())
 }
