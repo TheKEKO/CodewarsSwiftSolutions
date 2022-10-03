81418581fb12f2c6f74e3242a80bdd5bcc26f151
Aleksandr F <a7214370@gmail.com>
2022-10-03 17:08:21 +0300

    Create Is n divisible by x and y?.swift

---
+++ 8kyu/Is n divisible by x and y?.swift
@@ -0,0 +1,8 @@
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+// Создайте функцию, которая проверяет, делится ли число n на два числа x и y. Все входные данные - положительные ненулевые числа.
+
+func isDivisible(_ n: Int, _ x: Int, _ y: Int) -> Bool {
+    // your code here
+}
+
+// Solution
