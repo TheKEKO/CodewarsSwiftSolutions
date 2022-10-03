41ae024f318ee6d5de3f6f87508a26332f51abf2
Aleksandr F <a7214370@gmail.com>
2022-10-03 17:26:58 +0300

    Create Find the first non-consecutive number.swift

---
+++ 8kyu/Find the first non-consecutive number.swift
@@ -0,0 +1,34 @@
+/* Your task is to find the first element of an array that is not consecutive.
+By not consecutive we mean not exactly 1 larger than the previous element of the array.
+E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+
+
+Ваша задача - найти первый элемент массива, который не является последовательным.
+Под не последовательным мы понимаем элемент, который не больше предыдущего элемента массива ровно на 1.
+Например, если у нас есть массив [1,2,3,4,6,7,8], то 1, 2, 3, 4 - все последовательные, а 6 - нет, поэтому это первое не последовательное число. */
+
+func firstNonConsecutive (_ arr: [Int]) -> Int? {
+    return nil
+}
+
+// Solution 1
+func firstNonConsecutive (_ arr: [Int]) -> Int? {
+    var num = arr[0]
+    
+    for number in arr {
+        if number == num {
+            num += 1
+        } else {
+            return number
+        }
+    }
+    return nil
+}
+
+// Solution 2
+func firstNonConsecutive (_ arr: [Int]) -> Int? {
+    for i in 1..<arr.count {
+        if arr[i] - arr[i-1] > 1 { return arr[i] }
+    }
+    return nil
+}
