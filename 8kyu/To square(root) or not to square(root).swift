f9d5f58800239810f3148f11c605d43664d3e9d4
Aleksandr F <a7214370@gmail.com>
2022-10-04 12:21:25 +0300

    Create To square(root) or not to square(root).swift

---
+++ 8kyu/To square(root) or not to square(root).swift
@@ -0,0 +1,17 @@
+// To square(root) or not to square(root)
+
+/* Write a method, that will get an integer array as parameter and will process every number from this array.
+Return a new array with processing every number of the input-array like this:
+If the number has an integer square root, take this, otherwise square the number. 
+
+Напишите метод, который будет получать в качестве параметра целочисленный массив и обрабатывать каждое число из этого массива.
+Верните новый массив, обработав каждое число из входного массива следующим образом:
+Если число имеет целочисленный квадратный корень, извлеките его, в противном случае возведите число в квадрат.
+*/
+
+import Foundation
+
+func squareOrSquareRoot(_ input: [Int]) -> [Int] {
+}
+
+// Solution 
