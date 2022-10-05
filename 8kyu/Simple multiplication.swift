7b2f85a05fd44790d6986576eb28854760410425
Aleksandr F <a7214370@gmail.com>
2022-10-06 02:01:42 +0300

    Update Simple multiplication.swift

--- 8kyu/Simple multiplication.swift
+++ 8kyu/Simple multiplication.swift
@@ -2,3 +2,7 @@
 
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 // Это kata заключается в умножении заданного числа на восемь, если это четное число и на девять в противном случае.
+
+func simpleMultiplication(_ num: Int) -> Int {
+
+}
