42668a5d71255be8c346fef999f2af5ec5d39796
Aleksandr F <a7214370@gmail.com>
2022-10-05 23:38:11 +0300

    Create Grasshopper - Terminal game move function.swift

---
+++ 8kyu/Grasshopper - Terminal game move function.swift
@@ -0,0 +1,10 @@
+// Grasshopper - Terminal game move function
+
+/* Terminal game move function
+In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+Функция перемещения в терминальной игре
+В этой игре герой перемещается слева направо. Игрок бросает кубик и перемещается на количество мест, указанное на кубике, два раза.
+Создайте функцию для терминальной игры, которая принимает текущую позицию героя и бросок (1-6) и возвращает новую позицию. */
+
