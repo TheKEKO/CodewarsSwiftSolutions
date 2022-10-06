c216fc7dded9379abb4a3149cd1bd6c01e27bfe5
Aleksandr F <a7214370@gmail.com>
2022-10-06 03:19:35 +0300

    Update Grasshopper - Grade book.swift

--- 8kyu/Grasshopper - Grade book.swift
+++ 8kyu/Grasshopper - Grade book.swift
@@ -1,7 +1,7 @@
 // Grasshopper - Grade book
 
 // Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
-// Доработайте функцию так чтобы она находила среднее значение из трех переданных ей оценок и возвращала буквенное значение связанное с этой оценкой.
+// Доработайте функцию так, чтобы она находила среднее значение из трех переданных ей оценок и возвращала буквенное значение связанное с этой оценкой.
 
 func getGrade(_ s1: Int, _ s2: Int, _ s3: Int) -> String {
     return ""
