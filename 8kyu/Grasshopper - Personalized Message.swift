3ad223c0430cccd8c0ccbac2d46b365feb3f14b0
Aleksandr F <a7214370@gmail.com>
2022-10-07 13:42:30 +0300

    Update Grasshopper - Personalized Message.swift

--- 8kyu/Grasshopper - Personalized Message.swift
+++ 8kyu/Grasshopper - Personalized Message.swift
@@ -3,3 +3,6 @@
 // Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
 // Создайте функцию, которая выдает персональное приветствие. Эта функция принимает два параметра: имя и владелец.
 
+func great(_ name: String, _ owner: String) -> String {
+  // complete this function
+}
