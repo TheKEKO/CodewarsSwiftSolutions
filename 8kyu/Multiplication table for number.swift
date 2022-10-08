18bcd85781a2c597dc79519a9cab0117aa5a597e
Aleksandr F <a7214370@gmail.com>
2022-10-08 23:54:27 +0300

    Update Multiplication table for number.swift

--- 8kyu/Multiplication table for number.swift
+++ 8kyu/Multiplication table for number.swift
@@ -1,1 +1,4 @@
 // Multiplication table for number
+
+// Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+// Ваша цель - вернуть таблицу умножения для числа которое всегда является целым числом от 1 до 10.
