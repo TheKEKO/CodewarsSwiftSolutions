84921939f913f114c47344f4d98c376070aea18a
Aleksandr F <a7214370@gmail.com>
2022-10-08 23:55:04 +0300

    Update Multiplication table for number.swift

--- 8kyu/Multiplication table for number.swift
+++ 8kyu/Multiplication table for number.swift
@@ -2,3 +2,7 @@
 
 // Your goal is to return multiplication table for number that is always an integer from 1 to 10.
 // Ваша цель - вернуть таблицу умножения для числа которое всегда является целым числом от 1 до 10.
+
+func multi_table(_ number: Int) -> String {
+    return ""
+}
