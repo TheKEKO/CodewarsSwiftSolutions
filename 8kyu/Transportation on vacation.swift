d4d19f293678d3bb63a7019bbca13e2053581555
Aleksandr F <a7214370@gmail.com>
2022-10-12 12:10:14 +0300

    Update Transportation on vacation.swift

--- 8kyu/Transportation on vacation.swift
+++ 8kyu/Transportation on vacation.swift
@@ -6,3 +6,10 @@ You will need a rental car in order for you to get around in your vacation. The 
 Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. 
 Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 Write a code that gives out the total amount for different days(d).
+
+После тяжелого рабочего дня в офисе вы решили отдохнуть в отпуске. 
+Вы бронируете билеты на самолет для себя и своей девушки и пытаетесь оставить все неприятности позади.
+Чтобы передвигаться во время отпуска, вам понадобится арендованный автомобиль. Менеджер по прокату автомобилей делает вам выгодные предложения.
+Каждый день аренды автомобиля стоит 40 долларов. Если вы арендуете автомобиль на 7 и более дней, вы получаете скидку $50. 
+Или если вы арендуете автомобиль на 3 или более дней, вы получаете скидку $20.
+Напишите код, который выдает общую сумму за разные days(d). */
