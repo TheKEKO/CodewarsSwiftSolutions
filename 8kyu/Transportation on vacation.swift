a46c3a9b7506f83dc3828c84b844d55152fd7fee
Aleksandr F <a7214370@gmail.com>
2022-10-12 12:36:50 +0300

    Update Transportation on vacation.swift

--- 8kyu/Transportation on vacation.swift
+++ 8kyu/Transportation on vacation.swift
@@ -34,3 +34,17 @@ func RentalCarCost(_ days: Int) -> Int {
     var discount = days >= 7 ? 50 : 20
     return days >= 3 ? cost - discount : cost
 }
+
+// Solution 3
+func RentalCarCost(_ days: Int) -> Int {
+    let rentCostPerDay = 40
+    var total = days * rentCostPerDay
+    
+    if days >= 7 {
+        total -= 50
+    } else if days >= 3 {
+        total -= 20
+    }
+    
+    return total
+}
