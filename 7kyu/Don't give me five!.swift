20b2bd0fbf8417732c2b9d922d29d80678e16026
Aleksandr F <a7214370@gmail.com>
2022-10-12 17:07:26 +0300

    Create Don't give me five!.swift

---
+++ 7kyu/Don't give me five!.swift
@@ -0,0 +1,12 @@
+// Don't give me five!
+
+/* In this kata you get the start number and the end number of a region and should 
+return the count of all numbers except numbers with a 5 in it. 
+The start and the end number are both inclusive!
+
+В этом ката вы получаете начальный номер и конечный номер региона и должны 
+вернуть количество всех чисел, кроме чисел с цифрой 5. 
+Начальный и конечный номер являются инклюзивными! */
+
+func dontGiveMeFive(_ start: Int, _ end: Int) -> Int {
+}
