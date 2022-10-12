13a9c79b98c72f66f79610e9c27627a68ca03198
Aleksandr F <a7214370@gmail.com>
2022-10-12 17:08:36 +0300

    Update Don't give me five!.swift

--- 7kyu/Don't give me five!.swift
+++ 7kyu/Don't give me five!.swift
@@ -23,3 +23,8 @@ func dontGiveMeFive(_ start: Int, _ end: Int) -> Int {
     
     return numbersCount
 }
+
+// Solution 2
+func dontGiveMeFive(_ start: Int, _ end: Int) -> Int {
+    return (start...end).filter {!String($0).contains("5")}.count
+}
