297215733231436d08b09ea0bcf88866fda696a1
Aleksandr F <a7214370@gmail.com>
2022-10-13 10:14:46 +0300

    Update Get Planet Name By ID.swift

--- 8kyu/Get Planet Name By ID.swift
+++ 8kyu/Get Planet Name By ID.swift
@@ -2,3 +2,28 @@
 
 // The function is not returning the correct values. Can you figure out why?
 // Функция не возвращает правильные значения. Можете ли вы выяснить, почему?
+
+func getPlanetName(_ id: Int) -> String {
+  var name:String
+  switch id {
+    case 1:
+      name = "Mercury"
+    case 2:
+      name = "Venus"
+    case 3:
+      name = "Earth"
+    case 4:
+      name = "Mars"
+    case 5:
+      name = "Jupiter"
+    case 6:
+      name = "Saturn"
+    case 7:
+      name = "Uranus"
+    case 8:
+      name = "Neptune"
+    default:
+      name = ""
+    return name
+  }
+}
