cb4c99c71aada615e370de2f2f861404423b854a
Aleksandr F <a7214370@gmail.com>
2022-10-13 10:13:53 +0300

    Update Get Planet Name By ID.swift

--- 8kyu/Get Planet Name By ID.swift
+++ 8kyu/Get Planet Name By ID.swift
@@ -1,1 +1,4 @@
 // Get Planet Name By ID
+
+// The function is not returning the correct values. Can you figure out why?
+// Функция не возвращает правильные значения. Можете ли вы выяснить, почему?
