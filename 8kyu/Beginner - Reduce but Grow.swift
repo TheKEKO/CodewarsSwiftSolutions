4516e67119c1cd41bf92bc48bd5d0c6700207f4b
Aleksandr F <a7214370@gmail.com>
2022-10-14 17:12:12 +0300

    Create Beginner - Reduce but Grow.swift

---
+++ 8kyu/Beginner - Reduce but Grow.swift
@@ -0,0 +1,8 @@
+// Beginner - Reduce but Grow
+
+// Given a non-empty array of integers, return the result of multiplying the values together in order.
+// Учитывая непустой массив целых чисел, верните результат умножения этих значений по порядку.
+
+func grow(_ arr: [Int]) -> Int {
+
+}
