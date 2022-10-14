47251da12a82fd6b90758cd95bca7d7813f47019
Aleksandr F <a7214370@gmail.com>
2022-10-14 18:19:59 +0300

    Update Roman Numerals Encoder.swift

--- 6kyu/Roman Numerals Encoder.swift
+++ 6kyu/Roman Numerals Encoder.swift
@@ -10,3 +10,32 @@ or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI. */
 
 func solution(_ number:Int) -> String {
 }
+
+// Solution 
+func solution(_ number:Int) -> String {
+    let conversionTable: [(intNumber: Int, romanNumber: String)] =
+    [(1000, "M"),
+     (900, "CM"),
+     (500, "D"),
+     (400, "CD"),
+     (100, "C"),
+     (90, "XC"),
+     (50, "L"),
+     (40, "XL"),
+     (10, "X"),
+     (9, "IX"),
+     (5, "V"),
+     (4, "IV"),
+     (1, "I")]
+    
+    var roman = ""
+    var remainder = 0
+    
+    for entry in conversionTable {
+        let quotient = (number - remainder) / entry.intNumber
+        remainder += quotient * entry.intNumber
+        roman += String(repeating: entry.romanNumber, count: quotient)
+    }
+    
+    return roman
+}
