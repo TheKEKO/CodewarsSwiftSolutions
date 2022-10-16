43721c8a930a051131437563cc315f69b6bdf2af
Aleksandr F <a7214370@gmail.com>
2022-10-16 10:17:44 +0300

    Create Century From Year.swift

---
+++ 8kyu/Century From Year.swift
@@ -0,0 +1,8 @@
+// Century From Year
+
+/* The first century spans from the year 1 up to and including the year 100, 
+the second century - from the year 101 up to and including the year 200, etc. */
+
+func century(_ year: Int) -> Int {
+    // your code here
+}
