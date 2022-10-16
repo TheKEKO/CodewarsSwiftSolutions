0461bc734ecfcef982f387f46b5b8d1ad5f06f9f
Aleksandr F <a7214370@gmail.com>
2022-10-16 10:18:08 +0300

    Update Century From Year.swift

--- 8kyu/Century From Year.swift
+++ 8kyu/Century From Year.swift
@@ -6,3 +6,8 @@ the second century - from the year 101 up to and including the year 200, etc. */
 func century(_ year: Int) -> Int {
     // your code here
 }
+
+// Solution
+func century(_ year: Int) -> Int {
+    return (year + 99) / 100
+}
