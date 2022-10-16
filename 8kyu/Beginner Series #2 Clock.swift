03d67e37513f6844c2b1b47d459705e09ca16b6b
Aleksandr F <a7214370@gmail.com>
2022-10-16 10:51:28 +0300

    Create Beginner Series #2 Clock.swift

---
+++ 8kyu/Beginner Series #2 Clock.swift
@@ -0,0 +1,9 @@
+// Beginner Series #2 Clock
+
+/* Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds. */
+
+func past(_ h: Int, _ m: Int, _ s: Int) -> Int {
+    // your code here
+}
