f85c3e6b029b0cccecb40f8f021e4b1b0ea9bc59
Aleksandr F <a7214370@gmail.com>
2022-10-17 21:55:57 +0300

    Update Sum of positive.swift

--- 8kyu/Sum of positive.swift
+++ 8kyu/Sum of positive.swift
@@ -1,3 +1,8 @@
 // Sum of positive
 
 // You get an array of numbers, return the sum of all of the positives ones.
+
+func sumOfPositives (_ numbers: [Int] ) -> Int {
+    // Your code here
+    return 0
+}
