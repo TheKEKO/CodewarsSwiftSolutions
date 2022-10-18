143d69959654b00e0b8d79a26a7eafb6140eabc2
Aleksandr F <a7214370@gmail.com>
2022-10-18 10:02:31 +0300

    Update Sum of positive.swift

--- 8kyu/Sum of positive.swift
+++ 8kyu/Sum of positive.swift
@@ -6,3 +6,15 @@ func sumOfPositives (_ numbers: [Int] ) -> Int {
     // Your code here
     return 0
 }
+
+// Solution 1
+func sumOfPositives (_ numbers: [Int] ) -> Int {
+    var num = numbers
+    let arrayFilter = num.filter{$0 >= 0}
+    var result = arrayFilter.reduce(0, +)
+    if result > 1 {
+        return(result)
+    } else {
+        return 0
+    }
+}
