2d449bbdbbb3b6d06918023f33f72ebfb2fff1ff
Aleksandr F <a7214370@gmail.com>
2022-10-19 12:47:12 +0300

    Create Convert a Number to a String!.swift

---
+++ 8kyu/Convert a Number to a String!.swift
@@ -0,0 +1,11 @@
+// Convert a Number to a String!
+
+/*
+We need a function that can transform a number (integer) into a string.
+What ways of achieving this do you know?
+*/
+
+func numberToString(number: Int) -> String { 
+  // TODO Your code goes here!
+  return ""
+}
