6e3af24fa9ee20a8df88dafb745d5f6c7305730c
Aleksandr F <a7214370@gmail.com>
2022-10-19 12:52:03 +0300

    Update Grasshopper - Check for factor.swift

--- 8kyu/Grasshopper - Check for factor.swift
+++ 8kyu/Grasshopper - Check for factor.swift
@@ -1,6 +1,7 @@
 // Grasshopper - Check for factor
 
-/* Factors are numbers you can multiply together to get another number.
+/* 
+Factors are numbers you can multiply together to get another number.
 
 2 and 3 are factors of 6 because: 2 * 3 = 6
 
@@ -8,7 +9,8 @@ You can find a factor by dividing numbers. If the remainder is 0 then the number
 You can use the mod operator (%) in most languages to check for a remainder
 For example 2 is not a factor of 7 because: 7 % 2 = 1
 
-Note: base is a non-negative number, factor is a positive number. */
+Note: base is a non-negative number, factor is a positive number. 
+*/
 
 func checkForFactor(_ base: Int, _ factor: Int) -> Bool {
     return true
