e853159b2a1e9fd8c25834749669246921531c66
Aleksandr F <a7214370@gmail.com>
2022-10-19 12:50:08 +0300

    Update Century From Year.swift

--- 8kyu/Century From Year.swift
+++ 8kyu/Century From Year.swift
@@ -1,7 +1,9 @@
 // Century From Year
 
-/* The first century spans from the year 1 up to and including the year 100, 
-the second century - from the year 101 up to and including the year 200, etc. */
+/* 
+The first century spans from the year 1 up to and including the year 100, 
+the second century - from the year 101 up to and including the year 200, etc.
+*/
 
 func century(_ year: Int) -> Int {
     // your code here
