b8db2bca4b6886052a978f925b5f7280b10cde1c
Aleksandr F <a7214370@gmail.com>
2022-10-19 12:51:40 +0300

    Update Get Planet Name By ID.swift

--- 8kyu/Get Planet Name By ID.swift
+++ 8kyu/Get Planet Name By ID.swift
@@ -1,7 +1,6 @@
 // Get Planet Name By ID
 
 // The function is not returning the correct values. Can you figure out why?
-// Функция не возвращает правильные значения. Можете ли вы выяснить, почему?
 
 func getPlanetName(_ id: Int) -> String {
   var name:String
