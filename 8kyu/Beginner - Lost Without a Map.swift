f49add6f0bab3be638894ad59b86608b633c1ada
Aleksandr F <a7214370@gmail.com>
2022-10-21 10:23:46 +0300

    Create Beginner - Lost Without a Map.swift

---
+++ 8kyu/Beginner - Lost Without a Map.swift
@@ -0,0 +1,8 @@
+// Beginner - Lost Without a Map
+
+//Given an array of integers, return a new array with each value doubled.
+
+func maps(a : Array<Int>) -> Array<Int> {
+// write your code here...
+
+}
