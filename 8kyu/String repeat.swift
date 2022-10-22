457b2a166ff67008ef35812984e5162201809675
Aleksandr F <a7214370@gmail.com>
2022-10-22 14:40:46 +0300

    Create String repeat.swift

---
+++ 8kyu/String repeat.swift
@@ -0,0 +1,8 @@
+// String repeat
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+func repeatStr(_ n: Int, _ string: String) -> String {
+  // Code here:
+  return ""
+}
