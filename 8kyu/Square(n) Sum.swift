1d493ad12a5fa0d1599eadbe8f95119c66b531d2
Aleksandr F <a7214370@gmail.com>
2022-10-24 16:08:20 +0300

    Create Square(n) Sum.swift

---
+++ 8kyu/Square(n) Sum.swift
@@ -0,0 +1,7 @@
+// Square(n) Sum
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+func squareSum(_ vals: [Int]) -> Int {
+  
+}
