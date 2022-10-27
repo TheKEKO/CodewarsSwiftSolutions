ddb7b91928b88afef032147277c3555898c62641
Aleksandr F <a7214370@gmail.com>
2022-10-27 14:02:17 +0300

    Update Stringy Strings.swift

--- 8kyu/Stringy Strings.swift
+++ 8kyu/Stringy Strings.swift
@@ -25,3 +25,8 @@ func stringy(_ size: Int) -> String {
     }
     return result
 }
+
+// Solution 2
+func stringy(_ size: Int) -> String {
+  return (0..<size).map { $0 % 2 == 0 ? "1" : "0" }.joined()
+}
