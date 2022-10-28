8f13b78efcb9982c2d53e7abaec370e56590555f
Aleksandr F <a7214370@gmail.com>
2022-10-28 14:32:52 +0300

    Update Multiply.swift

--- 8kyu/Multiply.swift
+++ 8kyu/Multiply.swift
@@ -1,7 +1,6 @@
 // Multiply
 
 // This code does not execute properly. Try to figure out why.
-// Этот код не выполняется должным образом. Попробуйте выяснить причину.
 
 func multiply(_ a: Double, _ b: Double) -> Double {
     a / b
