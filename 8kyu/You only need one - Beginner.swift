a3b6751d6decd6928ecdc82593135cf44744d909
Aleksandr F <a7214370@gmail.com>
2022-10-28 14:36:04 +0300

    Update You only need one - Beginner.swift

--- 8kyu/You only need one - Beginner.swift
+++ 8kyu/You only need one - Beginner.swift
@@ -1,12 +1,10 @@
 // You only need one - Beginner
 
-/* You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+/* 
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
 The type of a and x can be String or Int.
 Return true if the array contains the value, false if not.
-
-Вам будет дан массив a и значение x. Все, что вам нужно сделать, это проверить, содержит ли предоставленный массив значение.
-Тип a и x может быть String или Int.
-Верните true, если массив содержит значение, false - если нет. */
+*/
 
 func check<T: Equatable>(_ a: [T], _ x: T) -> Bool {
     // Your code here
