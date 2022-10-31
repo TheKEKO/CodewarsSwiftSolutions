74d7a04785caf15ac534deb02737db6269cf21e0
Aleksandr F <a7214370@gmail.com>
2022-10-31 13:22:23 +0300

    Update Dollars and Cents.swift

--- 8kyu/Dollars and Cents.swift
+++ 8kyu/Dollars and Cents.swift
@@ -11,3 +11,6 @@ The rest of your team will make sure that the argument is sanitized before being
 to your function although you will need to account for adding trailing zeros if they are missing 
 (though you won't have to worry about a dangling period).
 */
+
+func formatMoney(_ val:Double) -> String {
+}
