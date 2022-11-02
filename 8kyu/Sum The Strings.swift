9a5b5b742491681cdf4701384fa82790950ff598
Aleksandr F <a7214370@gmail.com>
2022-11-02 12:00:40 +0300

    Update Sum The Strings.swift

--- 8kyu/Sum The Strings.swift
+++ 8kyu/Sum The Strings.swift
@@ -1,1 +1,10 @@
+// Sum The Strings
 
+/*
+Create a function that takes 2 integers in form of a string as an input, and outputs the 
+sum (also as a string).
+*/
+
+func sum_str(_ a:String, _ b:String) -> String {
+//     happy coding !
+}
