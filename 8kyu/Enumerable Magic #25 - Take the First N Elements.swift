c5c0fbc0e8254572a9afb3115991e040a131b88e
Aleksandr F <a7214370@gmail.com>
2022-11-08 17:07:50 +0300

    Update Enumerable Magic #25 - Take the First N Elements.swift

--- 8kyu/Enumerable Magic #25 - Take the First N Elements.swift
+++ 8kyu/Enumerable Magic #25 - Take the First N Elements.swift
@@ -9,3 +9,8 @@ an Array<Int> containing the first up to n elements from the array.
 func take(_ arr: [Int], _ n: Int) -> [Int] {
 
 }
+
+// Solution 1
+func take(_ arr: [Int], _ n: Int) -> [Int] {
+    return Array(arr.prefix(n))
+}
