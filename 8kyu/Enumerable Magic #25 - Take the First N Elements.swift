29d235c422c9e9d66ebfcc7cfe78774dc75585c2
Aleksandr F <a7214370@gmail.com>
2022-11-08 17:07:36 +0300

    Update Enumerable Magic #25 - Take the First N Elements.swift

--- 8kyu/Enumerable Magic #25 - Take the First N Elements.swift
+++ 8kyu/Enumerable Magic #25 - Take the First N Elements.swift
@@ -1,1 +1,11 @@
+// Swift
+// Enumerable Magic #25 - Take the First N Elements
 
+/*
+Create a function take that takes an Array<Int> and an Int, n, and returns 
+an Array<Int> containing the first up to n elements from the array.
+*/
+
+func take(_ arr: [Int], _ n: Int) -> [Int] {
+
+}
