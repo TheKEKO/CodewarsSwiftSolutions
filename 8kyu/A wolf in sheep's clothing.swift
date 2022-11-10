be3444faecc538cda03cca79d3c12f9a8015b15c
Aleksandr F <a7214370@gmail.com>
2022-11-10 09:03:49 +0300

    Update A wolf in sheep's clothing.swift

--- 8kyu/A wolf in sheep's clothing.swift
+++ 8kyu/A wolf in sheep's clothing.swift
@@ -7,21 +7,26 @@ Create a function take that takes an Array<Int> and an Int, n, and returns
 an Array<Int> containing the first up to n elements from the array.
 */
 
-func take(_ arr: [Int], _ n: Int) -> [Int] {
-
+func warnTheSheep(_ queue: [String]) -> String {
+  return ""
 }
 
 // Solution 1
-func take(_ arr: [Int], _ n: Int) -> [Int] {
-    return Array(arr.prefix(n))
+func warnTheSheep(_ queue: [String]) -> String {
+    let result = queue.count - (queue.firstIndex(of: "wolf") ?? 0) - 1
+    
+    if result == 0 {
+        return "Pls go away and stop eating my sheep"
+    } else {
+        return "Oi! Sheep number \(result)! You are about to be eaten by a wolf!"
+    }
 }
 
 // Solution 2
-func take(_ arr: [Int], _ n: Int) -> [Int] {
-    if arr.count >= n {
-        let result = Array(arr[0..<n])
-        return result
-    } else {
-        return arr
-    }
+func warnTheSheep(_ queue: [String]) -> String {
+    let result = queue.count - (queue.firstIndex(of: "wolf") ?? 0) - 1
+    
+    return result == 0
+    ? "Pls go away and stop eating my sheep"
+    : "Oi! Sheep number \(result)! You are about to be eaten by a wolf!"
 }
