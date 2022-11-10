aae7483b2cac576ee20ab272eb449afd4bfedd0d
Aleksandr F <a7214370@gmail.com>
2022-11-10 09:02:50 +0300

    Update A wolf in sheep's clothing.swift

--- 8kyu/A wolf in sheep's clothing.swift
+++ 8kyu/A wolf in sheep's clothing.swift
@@ -1,1 +1,8 @@
+// Swift
 
+// Enumerable Magic #25 - Take the First N Elements
+
+/*
+Create a function take that takes an Array<Int> and an Int, n, and returns 
+an Array<Int> containing the first up to n elements from the array.
+*/
