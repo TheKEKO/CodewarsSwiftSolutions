3a8f8c3260b1f4f87ebc3e630d57ae1129f8b405
Aleksandr F <a7214370@gmail.com>
2022-11-10 09:02:58 +0300

    Update A wolf in sheep's clothing.swift

--- 8kyu/A wolf in sheep's clothing.swift
+++ 8kyu/A wolf in sheep's clothing.swift
@@ -6,3 +6,7 @@
 Create a function take that takes an Array<Int> and an Int, n, and returns 
 an Array<Int> containing the first up to n elements from the array.
 */
+
+func take(_ arr: [Int], _ n: Int) -> [Int] {
+
+}
