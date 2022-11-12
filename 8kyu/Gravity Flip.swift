fb6b0db8a3bb2c1b407396ca36d2ea2ff419d60d
Aleksandr F <a7214370@gmail.com>
2022-11-12 14:20:29 +0300

    Update Gravity Flip.swift

--- 8kyu/Gravity Flip.swift
+++ 8kyu/Gravity Flip.swift
@@ -7,3 +7,8 @@ If you've completed this kata already and want a bigger challenge, here's the 3D
 Bob is bored during his physics lessons so he's built himself a toy box to help pass the time. 
 The box is special because it has the ability to change gravity.
 */
+
+func flip(_ direction: String, _ a: [Int]) -> [Int] {
+  // Do some magic
+  return []
+}
