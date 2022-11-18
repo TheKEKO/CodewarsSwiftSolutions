c56af29bca81c5fe52c7fd9f59312cd068a99983
Aleksandr F <a7214370@gmail.com>
2022-11-18 14:44:15 +0300

    Update Square Every Digit.swift

--- 7kyu/Square Every Digit.swift
+++ 7kyu/Square Every Digit.swift
@@ -1,1 +1,12 @@
+// Swift
+// Square Every Digit
 
+/*
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+Note: The function accepts an integer and returns an integer
+*/
+
+func squareDigits(_ num: Int) -> Int {
+  return 0
+}
