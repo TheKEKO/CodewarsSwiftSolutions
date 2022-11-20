73b2288fcbf28eb9b818b20d4f6456e481fc5436
Aleksandr F <a7214370@gmail.com>
2022-11-20 21:55:01 +0300

    Update Simple Fun #74: Growing Plant.swift

--- 7kyu/Simple Fun #74: Growing Plant.swift
+++ 7kyu/Simple Fun #74: Growing Plant.swift
@@ -1,1 +1,9 @@
+// Swift
+// Simple Fun #74: Growing Plant
 
+/*
+Task
+Each day a plant is growing by upSpeed meters. Each night that plant's height decreases by downSpeed 
+meters due to the lack of sun heat. Initially, plant is 0 meters tall. We plant the seed at the beginning 
+of a day. We want to know when the height of the plant will reach a certain level.
+*/
