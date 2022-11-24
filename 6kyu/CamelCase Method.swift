301de904ecdcd38dd9acc4155865578130a73f58
Aleksandr F <a7214370@gmail.com>
2022-11-24 12:05:16 +0300

    Update CamelCase Method.swift

--- 6kyu/CamelCase Method.swift
+++ 6kyu/CamelCase Method.swift
@@ -9,3 +9,8 @@ All words must have their first letter capitalized and all spaces removed.
 func camelCase(_ str: String) -> String {
 
 }
+
+// Solution
+func camelCase(_ str: String) -> String {
+    str.capitalized.components(separatedBy: " ").joined()
+}
