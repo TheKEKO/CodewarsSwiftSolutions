14405140db9b40b300327f279c7e2a35d6593032
Aleksandr F <a7214370@gmail.com>
2022-12-12 10:46:17 +0300

    Create Sum of angles.swift

---
+++ 7kyu/Sum of angles.swift
@@ -0,0 +1,8 @@
+// Swift
+// Sum of angles
+
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+func angle(_ n: Int) -> Int {
+    // your code here
+}
