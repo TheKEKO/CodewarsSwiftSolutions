8061a114b59efc757a26fc726acda814d5ac9b02
Aleksandr F <a7214370@gmail.com>
2022-12-12 10:34:49 +0300

    Update Sum of odd numbers.swift

--- 7kyu/Sum of odd numbers.swift
+++ 7kyu/Sum of odd numbers.swift
@@ -1,1 +1,13 @@
+// Swift
+// Sum of odd numbers
 
+/*
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+*/
