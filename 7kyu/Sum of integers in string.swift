7ca31ba15e66a8d31da065ba838a6181b5d0cadd
Aleksandr F <a7214370@gmail.com>
2023-02-24 22:26:29 +0300

    Update Sum of integers in string.swift

--- 7kyu/Sum of integers in string.swift
+++ 7kyu/Sum of integers in string.swift
@@ -8,3 +8,22 @@ For example, in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy
 func sumOfIntegersInString(_ string: String) -> Int {
     return 0
 }
+
+// Solution 
+func sumOfIntegersInString(_ string: String) -> Int {
+    var result = 0
+    var number = "0"
+    
+    for (index,char) in string.enumerated() {
+        if char.isNumber {
+            number += String(char)
+            if index == string.count - 1 {
+                result += Int(number) ?? 0
+            }
+        } else {
+            result += Int(number) ?? 0
+            number = "0"
+        }
+    }
+    return result
+}
