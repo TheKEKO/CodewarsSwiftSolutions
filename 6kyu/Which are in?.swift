c83187559f32ef5c87477d15a4e23429b4958b4c
Aleksandr F <a7214370@gmail.com>
2023-02-25 19:15:33 +0300

    Update Which are in?.swift

--- 6kyu/Which are in?.swift
+++ 6kyu/Which are in?.swift
@@ -1,1 +1,10 @@
+// Swift
+// Which are in?
+
+// Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+func inArray(_ a1: [String], _ a2: [String]) -> [String] {
+    // your code
+}
+
 
