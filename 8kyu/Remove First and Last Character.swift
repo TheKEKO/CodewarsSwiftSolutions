874277466659d2d45d3d2dcd57f7da8fd141655d
Aleksandr F <a7214370@gmail.com>
2024-04-19 20:34:28 +0300

    Update Remove First and Last Character.swift

--- 8kyu/Remove First and Last Character.swift
+++ 8kyu/Remove First and Last Character.swift
@@ -1,6 +1,8 @@
 // Remove First and Last Character
 
-// Solution 
+// Solution 1
 func removeFirstAndLast(_ str: String) -> String {
     return String(str.dropFirst().dropLast())
 }
+
+// Solution 2
